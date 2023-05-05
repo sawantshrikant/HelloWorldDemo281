48bd054cae2f236acc181eaae8dc23f7ceda1efb
Shrikant <sawantshrikant07@gmail.com>
2023-05-05 11:54:13 +0530

    [Shrikant] add:Confict resolve

--- HelloWorldDemo281Batch/Program.cs
+++ HelloWorldDemo281Batch/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Welcome to Batch 281");
+        Console.WriteLine("confict");
+
+    }
+}
